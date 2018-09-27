9954b7ca9b4f8f1d669b17a46213d94a3c13ef7e
sgobeyn <sgobeyn@purdue.edu>
2018-09-27 12:01:32 -0400

    Debugged program for lab 3

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -12,19 +12,22 @@ public class StringUtils {
             Ex: firstName = "Sean", lastName = "Flannery"
                     --> res = "SFlannery"
          */
-        String res = firstName.substring(0,1) + lastName;
+        String res = firstName.substring(0, 1) + lastName;
         /* Shorten the string to 8 characters
             Ex: res = "SFlannery"
                     --> res = "SFlanner"
          */
-        res.substring(0, 8);
+        if (res.length() > 8) {
+            res = res.substring(0, 8);
+        }
         /* Convert the name to lower-case
             Ex: res = "SFlanner"
                     --> res = "sflanner"
          */
-        res.toLowerCase();
+            res = res.toLowerCase();
         /* return the result of our calculation */
-        return res;
+            return res;
+
     }
 
     /**
@@ -36,6 +39,6 @@ public class StringUtils {
      */
     public static String replaceStudentUsername(String text, String username) {
         /* Replace every instance of the username with proper message */
-        return text.replaceAll(username, "[DATA EXPUNDGED]");
+        return text.replaceAll(username, "[DATA EXPUNGED]");
     }
 }
